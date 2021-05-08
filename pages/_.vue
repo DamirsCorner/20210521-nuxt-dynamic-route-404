f54eec85313ab7b00ad5aa8d97461af92fc3ef6f
Damir Arh <damir.arh@gmail.com>
2021-05-08 14:18:53 +0200

    Implement 404 response

---
+++ pages/_.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>URL: {{ path }}</div>
+    <nuxt-link to="/valid/not">nuxt link</nuxt-link>
+    <a href="/valid/not">HTML link</a>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+@Component({
+  middleware: 'validateRoute',
+  mounted(this: RedirectPage) {
+    this.path = this.$route.path
+  },
+})
+export default class RedirectPage extends Vue {
+  path: string = ''
+}
+</script>
